0c771714b97c6b63674ccb0780e0a44f8e26ee00
raheels88 <80966727+raheels88@users.noreply.github.com>
2021-08-16 22:45:54 +0100

    Add files via upload

---
+++ bootcampBot_1.py
@@ -0,0 +1,21 @@
+import discord					
+import os						
+
+client = discord.Client()		
+
+@client.event					
+async def on_ready():			
+    print('Beep Boop Beep...{0.user}'.format(client) + ' is online')
+
+@client.event
+async def on_message(message):					#on_message event - everything in this function is executed for EVERY message received
+  msg = message.content							#assign the variable msg to be the content of a message (a string)
+
+  if message.author == client.user:				#if the author of the message is the bot itself...
+    return										#...then do nothing
+
+  if msg.startswith('$hello'):					#if the message (sent by anyone but the bot) starts with '$hello'...
+    await message.channel.send('Hello!')		#...'await' suspends the function while the bot does something
+												#message.channel.send('Hello!') tells the bot to send 'Hello!' in the same channel the orignal message was sent in.
+
+client.run(os.getenv('TOKEN'))
